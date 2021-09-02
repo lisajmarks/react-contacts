13ccbb961cd866f900dc62b4bd7ed55134bd6c8e
Lisa <lisajmarks@pm.me>
2021-09-02 16:11:21 -0700

    take home

--- src/components/contacts/ContactCard.js
+++ src/components/contacts/ContactCard.js
@@ -1,20 +1,62 @@
-const ContactCard = ({ firstName='', lastName='', phoneNumber='', profilePic='' }) => {
-    return (
-        <div>
-            <div>
-                <div>
-                    <img src={profilePic} alt={`Profile of ${firstName} ${lastName}.`} />
-                </div>
-                <div>
-                    <p>{firstName} {lastName}</p>
-                    <p>{phoneNumber}</p>
-                </div>
-            </div>
-            <div>
-                <button>X</button>
-            </div>
-        </div>
-    )
-}
+import styled from "styled-components";
+
+const CardWrapper = styled.div`
+  display: flex;
+  align-items: center;
+  height: 200px;
+  width: 700px;
+  padding-right: 20px;
+  margin-bottom: 20px;
+  box-shadow: 0 6px 18px -9px rgba(0, 0, 0, 0.75);
+  background-color: white;
+
+  &:last-of-type {
+    margin-bottom: 0;
+  }
+`;
+
+const ContactAvatarWrapper = styled.div`
+  padding: 0 20px;
+  border-right: 1px solid lightgray;
+`;
+
+const ContactAvatar = styled.img`
+  display: inline-block;
+  width: 125px;
+  height: 125px;
+  border-radius: 50%;
+`;
+
+const CardBody = styled.div`
+  padding: 0 20px;
+  flex-grow: 1;
+`;
+
+const ContactCard = ({
+  firstName = "",
+  lastName = "",
+  phoneNumber = "",
+  profilePic = "",
+}) => {
+  return (
+    <CardWrapper>
+      <ContactAvatarWrapper>
+        <ContactAvatar
+          src={profilePic}
+          alt={`Profile of ${firstName} ${lastName}.`}
+        />
+      </ContactAvatarWrapper>
+      <CardBody>
+        <p>
+          {firstName} {lastName}
+        </p>
+        <p>{phoneNumber}</p>
+      </CardBody>
+      <div>
+        <button>X</button>
+      </div>
+    </CardWrapper>
+  );
+};
 
 export default ContactCard;
--- src/components/contacts/ContactsList.js
+++ src/components/contacts/ContactsList.js
@@ -1,19 +1,25 @@
-import ContactCard from "./ContactCard"
+import ContactCard from "./ContactCard";
+import Container from "react-bootstrap/Container";
+import styled from "styled-components";
 
-const ContactList = ({ contacts=[] }) => {
-    return (
-        <div>
-            {contacts.map((contact) => 
-                <ContactCard 
-                    key={contact.id}
-                    firstName={contact.firstName} 
-                    lastName={contact.lastName} 
-                    phoneNumber={contact.phoneNumber} 
-                    profilePic={contact.profilePic}
-                />
-            )}
-        </div>
-    )
-}
+const LightGreyContainer = styled(Container)`
+  background-color: rgba(0, 0, 0, 0.1);
+`;
+
+const ContactList = ({ contacts = [] }) => {
+  return (
+    <LightGreyContainer className="d-flex flex-column align-items-center py-5">
+      {contacts.map((contact) => (
+        <ContactCard
+          key={contact.id}
+          firstName={contact.firstName}
+          lastName={contact.lastName}
+          phoneNumber={contact.phoneNumber}
+          profilePic={contact.profilePic}
+        />
+      ))}
+    </LightGreyContainer>
+  );
+};
 
 export default ContactList;
